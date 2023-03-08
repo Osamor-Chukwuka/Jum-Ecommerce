8d29f569e71a2b22d743489680cfb04e140ad4a9
Osamor Chukwuka <osamorchukwuka15@gmail.com>
2023-03-08 15:52:32 +0100

    Integrated Paystack Account verification and reference_code API

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -91,6 +91,86 @@ class CartController extends Controller
         return view('checkout');
     }
 
+
+
+    public function verifySellerAccount()
+    {
+        $account_number = 6173644787;
+        $sort_codeee = '070';
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://api.paystack.co/bank/resolve?account_number=$account_number&bank_code=$sort_codeee",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_SSL_VERIFYHOST => 0,
+            CURLOPT_SSL_VERIFYPEER => 0,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_HTTPHEADER => array(
+                "Authorization: Bearer sk_test_ccc6e9fd27af032f21df6afa0c1e9a809a47da24",
+                "Cache-Control: no-cache",
+            ),
+        ));
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        if ($err) {
+            echo "cURL Error #:" . $err;
+        } else {
+            // check if account name == name provided by the seller
+            // echo $response;
+            return $this->generateRecipentCode('osamor chukwuka chukwunoye', $account_number, $sort_codeee);
+        }
+    }
+
+
+    public function generateRecipentCode($name, $account_number, $bank_code)
+    {
+        $url = "https://api.paystack.co/transferrecipient";
+
+        $fields = [
+            'type' => "nuban",
+            'name' => $name,
+            'account_number' => $account_number,
+            'bank_code' => $bank_code,
+            'currency' => "NGN"
+        ];
+
+        $fields_string = http_build_query($fields);
+
+        //open connection
+        $ch = curl_init();
+
+        //set the url, number of POST vars, POST data
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            "Authorization: Bearer sk_test_ccc6e9fd27af032f21df6afa0c1e9a809a47da24",
+            "Cache-Control: no-cache",
+        ));
+
+        //So that curl_exec returns the contents of the cURL; rather than echoing it
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        //execute post
+        $result = curl_exec($ch);
+        $result = json_decode($result);
+        $recipient_code =  $result->data->recipient_code;
+        echo $recipient_code;
+    }
+
+
+
+
     public function verify($reference)
     {
         $curl = curl_init();
@@ -120,9 +200,12 @@ class CartController extends Controller
         if ($err) {
             echo "cURL Error #:" . $err;
         } else {
-            return view('verify', [
-                'response' => $response
-            ]);
+            return $this->verifySellerAccount();
+
+
+            // return view('verify', [
+            //     'response' => $response
+            // ]);
         }
     }
 }
